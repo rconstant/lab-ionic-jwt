e62bc627fd1d711394faf39eec3598ce8fedd4f6
Romain CONSTANT <rconstant@e-synaps.com>
2018-10-25 21:38:28 +0200

    first commit

---
+++ src/app/config.ts
@@ -0,0 +1,9 @@
+export let cfg = {
+  apiUrl: 'http://127.0.0.1:8000',
+  tokenName: 'token',
+  user: {
+    register: '/auth/signup',
+    login: '/login_check'
+  },
+  books: '/books'
+};
---
+++ src/models/book.model.ts
@@ -0,0 +1,14 @@
+export class BookModel {
+
+  public id?: number;
+  // public user_id?: number;
+  public isbn: string;
+  public title: string;
+  public description: string;
+  public author: string;
+  public publication_date: Date
+  // public pages_count: string;
+  // public created_at?: string;
+  // public updated_at?: string;
+
+}
